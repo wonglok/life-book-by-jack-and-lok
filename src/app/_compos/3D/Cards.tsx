841765530c719a79972fd085aeb8594f21efddc0
Lok Lok <yellowhappy831@gmail.com>
2026-05-09 16:25:00 +0800

    save

--- src/app/_compos/3D/Cards.tsx
+++ src/app/_compos/3D/Cards.tsx
@@ -73,7 +73,7 @@ export function Cards({ state, setState }: ProverbsCardProps) {
 
       {state?.memories?.map((mem, idx) => {
         return (
-          <group key={mem._id + idx}>
+          <group position={[0, 0, 1]} key={mem._id + idx}>
             <Suspense fallback={null}>
               <OneCard mouse={refAccuTotal} info={mem}></OneCard>
             </Suspense>
